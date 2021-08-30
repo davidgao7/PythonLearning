3a937220aed6627d27a1911bb9768f949d2e39c3
David Gao <582435572@qq.com>
2021-08-30 16:40:11 +0800

    read user input as string without *prompts*

--- fileIO.py
+++ fileIO.py
@@ -5,3 +5,8 @@ print(file.mode)
 print(file.writable())
 file.write("this is a file")
 file.close()
+
+# read input without prompt
+# when call input(), python waits for user input
+x = input()
+print("you enter %s,I didn't prompt you anythin" % x)
